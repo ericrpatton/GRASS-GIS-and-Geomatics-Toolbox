819d9a10e9983fdac33e9b233288a10d97b613b1
ericrpatton <eric.r.patton@protonmail.com>
2022-01-28 16:53:58 -0400

    Remove garbage files

--- start_monerod.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-sudo iptables  -I OUTPUT 2 -p tcp -d 127.0.0.1 -m tcp --dport 18081 -j ACCEPT
-DNS_PUBLIC=tcp TORSOCKS_ALLOW_INBOUND=1 torsocks monerod --p2p-bind-ip 127.0.0.1 --no-igd --rpc-bind-ip 127.0.0.1 --db-salvage --ban-list block.txt
