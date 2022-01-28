3942ed03c715f0ce625e6c62a29c53c80cc71dfe
ericrpatton <eric.r.patton@protonmail.com>
2022-01-28 17:41:53 -0400

    get_grass_help.sh: Fixed some garbage that I found in the file

--- get_grass_help.sh
+++ get_grass_help.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-<<<<<<< HEAD:get_grass_help.sh
 # 
 # get_grass_help.sh: Prints the help message for the Grass GIS program given as
 # a parameter, without having to be in Grass.
@@ -8,15 +7,6 @@
 #
 ###############################################################################
 
-=======
-#
-# Script: get_grass_help.sh
-#
-# Purpose: Prints the help info from a particular Grass program while outside of
-# Grass itself.
-#
-###############################################################################
->>>>>>> 6390d1c0a8c2bfae2605e7cf7d05d207c4c7ce4f:gethelp.sh
 SCRIPT=$(basename $0)
 
 if [ "$#" -ne 1 -o "$1" == "-H" -o "$1" == "-h" -o "$1" == "--help" -o "$1" == "-help" ] ; then
