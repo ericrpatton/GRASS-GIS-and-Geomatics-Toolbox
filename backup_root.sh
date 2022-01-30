f66ae2b24d1616946977a758f8e1a91ba98fb491
ericrpatton <eric.r.patton@protonmail.com>
2022-01-30 18:31:03 -0400

    backup_root.sh: Renamed backup media name

--- backup_root.sh
+++ backup_root.sh
@@ -1,4 +1,4 @@
-rsync -avhzP --delete --log-file=/var/log/backup_daily.log --exclude-from=/home/epatton/coderepo/excludes  / /media/epatton/465GB_Backup/Daily
+rsync -avhzP --delete --log-file=/var/log/backup_daily.log --exclude-from=/home/epatton/coderepo/excludes  / /media/epatton/EP_ARCH_01/Daily
 
 # -a, --archive               archive mode
 # -v, --verbose
