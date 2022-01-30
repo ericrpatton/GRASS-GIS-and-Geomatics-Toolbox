14c351bde89191eea60363485aed6e3aef0672be
ericrpatton <eric.r.patton@protonmail.com>
2022-01-30 18:41:15 -0400

    Added new scipt print_locate_db_path.sh

---
+++ print_locate_db_path.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# print_locate_db_path.sh : A simple scipt to write out an 'ls' listing of
+# databases created by updatedb into a string that will be accepted by the
+# 'locate' command. This script is used to telling locate what databse file to
+# scan when searching for files.
+#
+# by Eric Patton
+#
+###############################################################################
+
+ls -1 *.db | awk 'BEGIN {ORS=":"} {print $0}' | sed 's/:$//'
+
+exit 0
