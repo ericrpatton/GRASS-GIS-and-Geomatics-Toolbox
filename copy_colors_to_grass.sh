4a4faf9fd4f46c18a9aac495531e4fee55e4bbaf
ericrpatton <eric.r.patton@protonmail.com>
2022-02-01 14:13:05 -0400

    copy_colors_to_grass.sh: Updated path to Grass installation

--- copy_colors_to_grass.sh
+++ copy_colors_to_grass.sh
@@ -2,6 +2,8 @@
 
 cd /home/epatton/coderepo/Official_Colorbars
 
-cp bof* rain* srtm* gem* ocean Official_UNB_Chance.cbar rain* srtm* /usr/local/grass78/etc/colors
+cp bof* rain* srtm* gem* ocean Official_UNB_Chance.cbar /usr/local/grass81/etc/colors 
+
+cd -
 
 exit 0
