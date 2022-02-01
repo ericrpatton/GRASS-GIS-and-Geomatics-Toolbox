875ea13a01a5a479a92e8e11782716598a0c5f61
ericrpatton <eric.r.patton@protonmail.com>
2022-02-01 17:59:41 -0400

    last_modified_thing.sh: Modified the behaviour of the script to only output the last modified regualar file, instead of including files and directories.

--- last_modified_thing.sh
+++ last_modified_thing.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 #
-# Prints out just the name of the last thing you modified in the currecnt
-# directory. This differs from 'ls -1r' in that only the last file modified is
-# printed, not a full directory listing.
+# Prints out just the name of the most-recently modified regular file in the 
+# current directory. Note this script only prints files, not directories.
 #
-# Created: February 1, 2021
-# Modified: January 27, 2022
+# Created: January 27, 2022
+# Modified: February 1, 2021
 #
 # by Eric Patton
 #
@@ -14,12 +13,12 @@
 SCRIPT=$(basename $0)
 
 if [ "$1" == "-H" -o "$1" == "-h" -o "$1" == "--help" -o "$1" == "-help" ] ; then
-	echo -e "\n$SCRIPT: Prints out just the name of the last thing you modified in the currecnt directory. This differs from 'ls -1r' in that only the last file modified is printed, not a full directory listing."
+	echo -e "\n$SCRIPT: Prints out just the name of the most-recently modified regular file in the currecnt directory."
 	exit 1
 fi
 
 PATTERN=$1
 
-ls -ltrh ${PATTERN} | tail -n1 | awk '{print $9}'
+ls -lprt ${PATTERN} | grep -v / | tail -n1 | awk '{print $9}'
 
 exit 0
