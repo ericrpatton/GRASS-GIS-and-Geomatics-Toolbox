4f9c4a7e8913b7c96423e00d0bb94ff8a8ef0f7d
ericrpatton <eric.r.patton@protonmail.com>
2022-02-14 12:51:50 -0400

    mbsystem/mb.grid_this_line.sh: Renamed  to match the version generated by make_filenames.sh

--- mbsystem/mb.grid_this_line.sh
+++ mbsystem/mb.grid_this_line.sh
@@ -26,19 +26,21 @@ if [ "$#" -gt 2 -o "$1" == "-H" -o "$1" == "-h" -o "$1" == "--help" -o "$1" == "
 	exit 1
 fi
 
+# Input is an MB-System raw or processed file
 INPUT=$1
 RES=$2
 
 # Assign a default raster resolution if none was given on the command line.
 [[ -z ${RES} ]] && RES=10
 
-OUTPUT=`echo $INPUT | cut -f1 -d'.'`
-XYZ_OUTPUT=${OUTPUT}.xyz
+# Source make_filenames.sh, a script which handles all the filename wrangling
+[[ -f /home/epatton/coderepo/make_filenames.sh ]] && set -- $INPUT && . /home/epatton/coderepo/make_filenames.sh
+
 FORMAT_ID=`mbformat -I $INPUT -K | awk '{print $2}'`	
 
-mblist -F${FORMAT_ID} -I ${INPUT} -D2 | awk '{print $1, $2, -$3}' | proj `g.proj -jf` | pv | awk '{print $1, $2, $3}' > ${XYZ_OUTPUT}
+mblist -F${FORMAT_ID} -I ${INPUT} -D2 | awk '{print $1, $2, -$3}' | proj `g.proj -jf` | pv | awk '{print $1, $2, $3}' > ${OUTPUT_XYZ}
 
-import_xyz.sh ${XYZ_OUTPUT} ${RES}
+import_xyz.sh ${OUTPUT_XYZ} ${RES}
 r.colors map=${OUTPUT}_${RES}m color=bof_unb --v
 r.csr ${OUTPUT}_${RES}m
 
