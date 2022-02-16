5c4e3cb551f89af4b1a0a1bd45379114a2c029b3
ericrpatton <eric.r.patton@protonmail.com>
2022-02-15 23:35:48 -0400

    Updated my email address and copyright and last modified dates; Changed help exit code from 1 to 0.

--- script_preamble.sh
+++ script_preamble.sh
@@ -5,18 +5,18 @@
 # MODULE:        script_preamble.sh
 # 
 # AUTHOR:   	 Eric Patton, Geological Survey of Canada (Atlantic)
-# 		         <Eric dot Patton at Canada dot ca>
+# 		         <Eric dot Patton at NRCan dash RNCan dot gc dot ca>
 #
 # PURPOSE:       This is a collection of script fragments that I use over and
 # over, which should be sourced in scripts they are needed in.
 #
-# COPYRIGHT:     (c) 2021-2022 by Eric Patton
+# COPYRIGHT:     (c) 2022 by Eric Patton
 #
 #                This program is free software under the GNU General Public
 #                License (>=v3). 
 # 
 # Created:		  January 27, 2022
-# Last Modified:  January 27, 2022
+# Last Modified:  February 14, 2022
 #
 #############################################################################
 
@@ -24,7 +24,7 @@ SCRIPT=$(basename $0)
 
 if [ "$#" -ne 1 -o "$1" == "-H" -o "$1" == "-h" -o "$1" == "--help" -o "$1" == "-help" ] ; then
 	echo -e "\nusage: $SCRIPT XXXXXXXX \n"
-	exit 1
+	exit 0
 fi
 
 # Source make_filenames.sh, a script which handles all the filename wrangling
@@ -39,5 +39,3 @@ exitprocedure()
 
 # Setup clean exit for Ctrl-C or similar breaks.
 trap 'exitprocedure' 2 3 15
-
-
