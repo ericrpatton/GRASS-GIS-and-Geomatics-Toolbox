7afa6e9f6dee79e136f58b7176fe4a4833415b5b
ericrpatton <eric.r.patton@protonmail.com>
2023-03-10 15:48:12 -0400

    Updated script to Grass 8.3

--- copy_colors_to_grass.sh
+++ copy_colors_to_grass.sh
@@ -2,7 +2,7 @@
 
 cd /home/epatton/coderepo/Official_Colorbars
 
-cp bof* rain* srtm* gem* ocean Official_UNB_Chance.cbar /usr/local/grass81/etc/colors 
+cp bof* rain* srtm* gem* ocean Official_UNB_Chance.cbar /usr/local/grass83/etc/colors 
 
 cd -
 
