24211dea5826379caa73fca89e05fd428eab9b80
ericrpatton <eric.r.patton@protonmail.com>
2023-03-13 12:14:00 -0300

    Updated copyright and modification times, made output quieter

--- grass/get_gmt_region.sh
+++ grass/get_gmt_region.sh
@@ -9,14 +9,14 @@
 # PURPOSE:       To query the current region and print the bounds in
 #				 Geographic coordinates as input into GMT. 
 #
-# COPYRIGHT:     (C) 2013-2017 by Eric Patton 
+# COPYRIGHT:     (C) 2013-2022 by Eric Patton 
 #				 (Eric dot Patton at Canada dot ca)
 #
 #                This program is free software under the GNU General Public
 #                License (>=v3). Read the file COPYING that comes with GRASS
 #                for details.
 #
-# Last Modified: March 30, 2017
+# Last Modified: Jun 20, 2022
 #
 #############################################################################
 
@@ -30,7 +30,7 @@ SCRIPT=`basename $0`
 # Setup clean exit for Ctrl-C or similar breaks.
 trap 'echo -e "\n\n$SCRIPT: User break or similar caught; Exiting.\n" ; exit 1' 2 3 15
 
-g.region -bg > REGION.txt
+g.region -bg 2>/dev/null > REGION.txt
 
 WEST=`grep ll_w < REGION.txt | cut -d= -f2`
 EAST=`grep ll_e < REGION.txt | cut -d= -f2`
