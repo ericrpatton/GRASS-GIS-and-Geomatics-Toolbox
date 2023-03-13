1460774c19a18954b8f401e7608c87b48d55806f
ericrpatton <eric.r.patton@protonmail.com>
2023-03-13 13:34:37 -0300

    Made the message output more generic, and changed the script to use mpv to play output

--- job_failed_alarm.sh
+++ job_failed_alarm.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
-notify-send "GRASS GIS" "Process FAILED" && beep -f 440 -l 350 ; beep -f 420 -l 350 ; beep -f 400 -l 350 ; beep -f 380 -l 850 
+# If there is an internal PC speaker, the command below should work.
+#notify-send "GRASS GIS" "Process FAILED" && beep -f 440 -l 350 ; beep -f 420 -l 350 ; beep -f 400 -l 350 ; beep -f 380 -l 850 
+
+notify-send "Process FAILED" && mpv --no-terminal --no-audio-display /home/epatton/coderepo/Sounds/16_-_Game_Over.mp3 
 
 exit 0
