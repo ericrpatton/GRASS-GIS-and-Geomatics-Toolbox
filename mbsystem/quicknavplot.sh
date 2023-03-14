131067b760dd80335dd5a9381f034769efe5376f
ericrpatton <eric.r.patton@protonmail.com>
2023-03-14 12:14:28 -0300

    Added new script

--- mbsystem/quicknavplot.sh
+++ mbsystem/quicknavplot.sh
@@ -20,11 +20,17 @@ trap 'exitprocedure' 2 3 15
 
 if [ "$#" -eq 0 -o "$#" -gt 2 -o "$1" == "-H" -o "$1" == "-h" -o "$1" == "--help" -o "$1" == "-help" ] ; then
 	echo -e "\nusage: $SCRIPT DATALIST [REGION (w/e/s/n)]"
-	exit 1
+	echo "Use with the raw datalist.mb-1, *not* datalistp.mb-1!"
+	exit 0
 fi
 
 DATALIST=${1}
 PROCESSED_DATALIST="$(basename ${DATALIST} .mb-1)p.mb-1"
+PLOTNAME="$(basename $DATALIST .mb-1)_Navplot"
+CMD_NAME="${PLOTNAME}.cmd"
+PSNAME="${PLOTNAME}.ps"
+PDFNAME="$(basename $PSNAME .ps).pdf"
+
 [[ ! -f "${PROCESSED_DATALIST}" ]] && mbdatalist -F-1 -I ${DATALIST} -Z
 
 [[ -z ${2} ]] && REGION=$(mb.getinforegion $DATALIST) || REGION=$2 
@@ -32,6 +38,10 @@ PROCESSED_DATALIST="$(basename ${DATALIST} .mb-1)p.mb-1"
 echo -e "\nRegion is $REGION."
 sleep 2
 
-mbm_plot -F-1 -I ${DATALIST} -N -R${REGION} -V -MGU/-0.75/-1.75 -T -MTW0.25 -MTDf -MTG222/184/135 -MTN1 -MTN2 -L" " -O "$(basename $DATALIST .mb-1)_Navplot"
+mbm_plot -F-1 -I ${PROCESSED_DATALIST} -N -R${REGION} -V -MGU/-0.75/-2 -T -MTW0.25 -MTDf -MTG222/184/135 -MTN1 -MTN2 -L" " -O ${PLOTNAME} 
+bash ./${CMD_NAME} && sleep 2 && ps2pdf ${PSNAME} > ${PDFNAME}
+
+[[ -f ${PSNAME} ]] && rm ${PSNAME}
+[[ -f ${CMD_NAME} ]] && rm ${CMD_NAME}
 
 exit 0
